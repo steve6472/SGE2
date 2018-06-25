ebf7e9b1e9f82eb46e69d5bcb5f31d283a57d9ff
steve6472 <steve64959@gmail.com>
2018-06-25 13:39:11 +0200

    Formated font shader

--- src/com/steve6472/sge/gfx/Font.java
+++ src/com/steve6472/sge/gfx/Font.java
@@ -165,28 +165,39 @@ public class Font
 		this.screen = screen;
 		font = new Sprite("font.png");
 		invertedFont = new Sprite("invertedFont.png");
-		String fs = "#version 330 core\n"
-				+ "uniform sampler2D sampler;\n"
-				+ "uniform vec2 texture;\n"
-				+ "in vec4 vColor;\n"
-				+ "in vec2 vTexture;\n"
-				+ "out vec4 fragColor;\n"
-				+ "void main()\n"
-				+ "{\n"
-				+ "fragColor = texture2D(sampler, vTexture + texture) + vColor;\n"
+		String fs = 
+				  "#version 330 core													\n"
+				
+				+ "uniform sampler2D sampler;											\n"
+				+ "uniform vec2 texture;												\n"
+				+ "uniform vec4 col;													\n"
+				
+				+ "in vec4 vColor;														\n"
+				+ "in vec2 vTexture;													\n"
+				
+				+ "out vec4 fragColor;													\n"
+				
+				+ "void main()															\n"
+				+ "{																	\n"
+				+ "		fragColor = texture2D(sampler, vTexture + texture) + vColor + col;	\n"
 				+ "}";
-		String vs = "#version 330 core\n"
-				+ "layout(location = 0) in vec2 position;\n"
-				+ "layout(location = 1) in vec2 texture;\n"
-				+ "layout(location = 2) in vec4 color;\n"
-				+ "uniform mat4 projection;\n"
-				+ "out vec4 vColor;\n"
-				+ "out vec2 vTexture;\n"
-				+ "void main()\n"
-				+ "{\n"
-				+ "vColor = color;\n"
-				+ "vTexture = texture;\n"
-				+ "gl_Position = projection *  vec4(position, 0.0, 1.0);\n"
+		
+		String vs = "#version 330 core													\n"
+				
+				+ "layout(location = 0) in vec2 position;								\n"
+				+ "layout(location = 1) in vec2 texture;								\n"
+				+ "layout(location = 2) in vec4 color;									\n"
+				
+				+ "uniform mat4 projection;												\n"
+				
+				+ "out vec4 vColor;														\n"
+				+ "out vec2 vTexture;													\n"
+				
+				+ "void main()															\n"
+				+ "{																	\n"
+				+ "		vColor = color;													\n"
+				+ "		vTexture = texture;												\n"
+				+ "		gl_Position = projection *  vec4(position, 0.0, 1.0);			\n"
 				+ "}";
 		fontShader = new Shader(vs, fs);
 		fontShader.setUniform1f("sampler", 0);
